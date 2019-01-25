8b532a99270237d55fd67ab539f1fa0926a3c940
GEETHApriyaS5 <46866498+GEETHApriyaS5@users.noreply.github.com>
2019-01-25 17:32:46 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+scanf("%d",&n);
+if(n==0)
+{
+printf("zero");
+}
+else if(n>0)
+{
+printf("positive");
+}
+else
+{
+printf("negative");
+}
+getch();
+}
