21c1e1bcf031d1608a0fdc32bcd2331a782e08f9
GEETHApriyaS5 <46866498+GEETHApriyaS5@users.noreply.github.com>
2019-01-26 22:00:35 +0530

    Create mul.c

---
+++ mul.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,i=0,j=0;
+    scanf("%d",&n);
+    for(i=0;i<5;i++)
+    {   
+        j=j+n;
+        printf("%d",j);
+        
+    }
+    getch();
+}
