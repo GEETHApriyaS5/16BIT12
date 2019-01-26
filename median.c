526cfd3f4465f369a9de2d89e3f0348ce0d7708e
GEETHApriyaS5 <46866498+GEETHApriyaS5@users.noreply.github.com>
2019-01-26 23:10:11 +0530

    Create median.c

---
+++ median.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a[10],i,m=0,n,j,temp=0,n1,s;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {  m=a[0];
+    for(j=i+1;j<n;j++)
+    {
+        if(a[i]>a[j])
+        {
+            temp=a[i];
+            a[i]=a[j];
+            a[j]=temp;
+        }
+    }}
+    for(i=0;i<n;i++)
+    {
+    //printf("%d",a[i]);
+    n1=n/2;
+    s=a[n1];
+    }
+    printf("%d",s);
+    
+    getch();
+    
+}
