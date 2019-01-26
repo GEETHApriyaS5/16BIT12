98050d0325182dbbb1e382f95e5026d0c6451366
GEETHApriyaS5 <46866498+GEETHApriyaS5@users.noreply.github.com>
2019-01-26 22:49:09 +0530

    Create armax.c

---
+++ armax.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a[10],i,m=0,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {  m=a[0];
+        if(a[i]>m)
+        {
+            m=a[i];
+        }
+    }
+    printf("%d",m);
+    getch();
+    
+}
