3caa89bfd071e9d3bea03c2506999b06c21baa18
GEETHApriyaS5 <46866498+GEETHApriyaS5@users.noreply.github.com>
2019-01-27 12:01:06 +0530

    Create pow2.c

---
+++ pow2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n;
+    scanf("%d",&n);
+    int t,flag=0;
+    if(n%2==0)
+    {
+        printf("yes");
+    
+    }
+    else
+    {
+        printf("no");
+    }
+    getch();
+}
