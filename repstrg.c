0aed7159eaa92486fe71824d28c90b2aa751924b
GEETHApriyaS5 <46866498+GEETHApriyaS5@users.noreply.github.com>
2019-01-27 10:56:11 +0530

    Create repstrg.c

---
+++ repstrg.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+//void repe(char ,int);
+void main()
+{
+    int k,i;
+    char a[200];
+    scanf("%[^\n]",a);
+    scanf("%d",&k);
+    
+
+for(i=0;i<k;i++)
+{
+    printf("\n%s",a);
+}
+getch();
+}
