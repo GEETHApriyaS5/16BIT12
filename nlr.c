a7a43412e89e0739fc08c0c5366a254626bba6e6
GEETHApriyaS5 <46866498+GEETHApriyaS5@users.noreply.github.com>
2019-02-11 02:09:00 -0800

    Create nlr.c

---
+++ nlr.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,l,r,c=0;
+scanf("%d",&n);
+scanf("%d%d",&l,&r);
+if(n>l && n<r)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+getch();
+}
