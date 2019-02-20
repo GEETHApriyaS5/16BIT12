452d76b6abcf381db101ac6a0313877e47223be3
GEETHApriyaS5 <46866498+GEETHApriyaS5@users.noreply.github.com>
2019-02-20 02:24:08 -0800

    Rename rshiftri.c to shrigh.c

---
+++ shrigh.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,k,a[100],t,i,j;
+    scanf("%d%d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<k;i++)
+    {
+        t=a[n-1];
+        for(j=n-1;j>0;j--)
+        {
+            a[j]=a[j-1];
+        }
+        a[0]=t;
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d",a[i]);
+    }
+    getch();
+}
